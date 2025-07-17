4e8f796e20c4adaf152888183b70e046cfc9399a
hello-im-404 <frankebovich@gmail.com>
2025-07-17 18:07:23 -0400

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
-mkdir ~/.config/nvim
-mv ./init.lua ~/.config/nvim/
+git clone https://github.com/hello-im-404/nvim-dot.git ~/.config/nvim
+rm -rf ~/.config/nvim/README.md
+rm -rf ~/.config/nvim/screenshots
